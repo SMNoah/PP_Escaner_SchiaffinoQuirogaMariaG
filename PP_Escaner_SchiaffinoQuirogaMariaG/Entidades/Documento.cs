85af2cb898e2f9206510523a925fee6e1b5923b0
Maria Schiaffino <schiaffnm@gmail.com>
2024-06-13 15:48:52 -0300

    primer subida  a  github

---
+++ PP_Escaner_SchiaffinoQuirogaMariaG/Entidades/Documento.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Documento
+    {
+        int anio;
+        string autor;
+        string barcode;
+        Paso estado;
+        string numNormalizado;
+        string titulo;
+
+        public enum Paso
+        {
+            Inicio,
+            Distribuido,
+            EnEscaner,
+            EnRevision,
+            Terminado
+        }
+
+        protected Documento(int anio, string autor, string barcode, string numNormalizado, string titulo)
+        { 
+            this.anio = anio;
+            this.autor = autor;
+            this.barcode = barcode;
+            this.estado = Paso.Inicio;
+            this.numNormalizado = numNormalizado;
+            this.titulo = titulo;
+        }
+
+        public int Anio { get => anio; }
+        public string Autor { get => autor; }
+        public string Barcode { get => barcode; }
+        public Paso Estado { get => estado; }
+        protected string NumNormalizado { get => numNormalizado; }
+        public string Titulo { get => titulo; }
+
+        public bool AvanzarEstado()
+        {
+            switch (this.estado)
+            {
+                case Paso.Inicio:
+                    this.estado = Paso.Distribuido;
+                    return true;
+                case Paso.Distribuido:
+                    this.estado = Paso.EnEscaner;
+                    return true;
+                case Paso.EnEscaner:
+                    this.estado = Paso.EnRevision;
+                    return true;
+                case Paso.EnRevision:
+                    this.estado = Paso.Terminado;
+                    return true;
+                case Paso.Terminado:
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strb = new StringBuilder();
+
+            strb.AppendLine("");
+            strb.AppendLine($"Titulo: {this.titulo}");
+            strb.AppendLine($"Autor: {this.autor}");
+            strb.AppendLine($"Año: {this.anio}");
+            strb.AppendLine($"Cód. de barras: {this.barcode}");
+
+            return strb.ToString();
+        }
+
+
+    }
+}
---
+++ PP_Escaner_SchiaffinoQuirogaMariaG/Entidades/Libro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Libro : Documento
+    {
+        int numPaginas;
+
+        public Libro(int anio, string autor, string barcode, string numNormalizado, string titulo, int numPaginas) : base(anio, autor, barcode, numNormalizado, titulo)
+        {
+            this.numPaginas = numPaginas;
+        }
+
+        public int NumPaginas { get => numPaginas; }
+        public string ISBN { get => NumNormalizado; }
+
+        public static bool operator ==(Libro LibroUno, Libro LibroDos)
+        {
+            if (LibroUno.Barcode == LibroDos.Barcode || LibroUno.ISBN == LibroDos.ISBN || (LibroUno.Titulo == LibroDos.Titulo && LibroUno.Autor && LibroDos.Autor))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Libro LibroUno, Libro LibroDos)
+        {
+            return !(LibroUno == LibroDos);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strb = new StringBuilder();
+
+            strb.Append(base.ToString());
+            strb.AppendLine($"Número de páginas: {this.numPaginas}");
+
+            return strb.ToString();
+        }
+
+    }
+}
